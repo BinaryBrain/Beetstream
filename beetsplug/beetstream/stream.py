296fa02fa0c9385410ae6007e818f441a9de97ae
Brendan Taylor <brendan@stripe.com>
2022-12-13 15:09:24 -0700

    if ffmpeg-python is not available just send the raw file

--- beetsplug/beetstream/stream.py
+++ beetsplug/beetstream/stream.py
@@ -1,12 +1,19 @@
 from beetsplug.beetstream.utils import path_to_content_type
 from flask import send_file, Response
 
-import ffmpeg
+import importlib
+have_ffmpeg = importlib.util.find_spec("ffmpeg") is not None
+
+if have_ffmpeg:
+    import ffmpeg
 
 def send_raw_file(filePath):
     return send_file(filePath, mimetype=path_to_content_type(filePath))
 
 def transcode_and_stream(filePath, maxBitrate):
+    if not have_ffmpeg:
+        raise RuntimeError("Can't transcode, ffmpeg-python is not available")
+
     outputStream = (
         ffmpeg
         .input(filePath)
@@ -17,4 +24,7 @@ def transcode_and_stream(filePath, maxBitrate):
     return Response(outputStream.stdout, mimetype='audio/mpeg')
 
 def try_to_transcode(filePath, maxBitrate):
-    return transcode_and_stream(filePath, maxBitrate)
+    if have_ffmpeg:
+        return transcode_and_stream(filePath, maxBitrate)
+    else:
+        return send_raw_file(filePath)
